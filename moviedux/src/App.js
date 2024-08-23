db8dce40380fc2ab6d926cb2a4cbeffe796f8bfe
jonhealy1 <jonathan.d.healy@gmail.com>
2024-08-23 19:53:47 +0800

    add public files

--- moviedux/src/App.js
+++ moviedux/src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLOOOOOO WORLD!
-          How are you?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Wooooooooo!
-        </a>
+      <header className="header">
+        <h1>Welcome to Moviedux!!!!!!!!!!!!</h1>
       </header>
+      <footer className="footer">
+        <p className="footer">
+          Footer is here!
+        </p>
+      </footer>
     </div>
   );
 }
