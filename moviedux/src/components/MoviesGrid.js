f92238bf113d2b519a4b3e1c1149bef7d83b5346
jonhealy1 <jonathan.d.healy@gmail.com>
2024-08-24 20:36:47 +0800

    add genre, rating

--- moviedux/src/components/MoviesGrid.js
+++ moviedux/src/components/MoviesGrid.js
@@ -18,6 +18,8 @@ export default function MoviesGrid(){
                          <img src={`images/${movie.image}`} alt={movie.title}/>
                          <div className='movie-card-info'>
                             <h3 className='movie-card-title'>{movie.title}</h3>
+                            <p className='movie-card-genre'>{movie.genre}</p>
+                            <p className='movie-card-rating'>{movie.rating}</p>
                          </div>
                     </div>
                 ))
