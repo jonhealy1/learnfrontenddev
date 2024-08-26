486d2d13e68cfcb8c1e613f8f597eba14fea4f89
jonhealy1 <jonathan.d.healy@gmail.com>
2024-08-26 15:52:52 +0800

    change font color on rating

--- moviedux/src/App.js
+++ moviedux/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import './styles.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import MoviesGrid from './components/MoviesGrid'
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
+      <div className="container">
         <Header></Header>
         <MoviesGrid></MoviesGrid>
       </div>
