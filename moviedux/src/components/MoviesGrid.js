b81b90ab5fa0d3770b3e60c1a6db49df1c41eaef
jonhealy1 <jonathan.d.healy@gmail.com>
2024-08-27 15:52:26 +0800

    create filter bar

--- moviedux/src/components/MoviesGrid.js
+++ moviedux/src/components/MoviesGrid.js
@@ -6,6 +6,9 @@ export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
+  const [genre, setGenre] = useState("All genres");
+  const [rating, setRating] = useState("All");
+
   useEffect(() => {
     fetch("movies.json")
       .then((response) => response.json())
@@ -29,6 +32,29 @@ export default function MoviesGrid() {
         value={searchTerm}
         onChange={handleSearchChange}
       />
+
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select className="filter-dropdown">
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
+        </div>
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select className="filter-dropdown">
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
+        </div>
+      </div>
+
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
           <MovieCard movie={movie} key={movie.id}></MovieCard>
