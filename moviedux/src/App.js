f4ab631f4cd18e12b13e1d253798feb59388278f
jonhealy1 <jonathan.d.healy@gmail.com>
2024-08-30 06:58:17 +0800

    lifting movie state

--- moviedux/src/App.js
+++ moviedux/src/App.js
@@ -5,8 +5,17 @@ import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
 import Watchlist from "./components/Watchlist";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
@@ -24,7 +33,7 @@ function App() {
             </ul>
           </nav>
           <Routes>
-            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/" element={<MoviesGrid movies={movies} />}></Route>
             <Route path="/watchlist" element={<Watchlist />}></Route>
           </Routes>
         </Router>
