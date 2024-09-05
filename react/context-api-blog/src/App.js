ebdcb26e0303a772738266e98f30f44229bb0e4d
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-05 13:45:26 +0800

    add context to app

--- react/context-api-blog/src/App.js
+++ react/context-api-blog/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import UserInfoContext from "./context/UserInfoContext";
 
 function App() {
-  return <div></div>;
+  const userInfo = { username: "Admin", isAdmin: true };
+
+  return (
+    <div>
+      <UserInfoContext.Provider value={userInfo}></UserInfoContext.Provider>
+    </div>
+  );
 }
 
 export default App;
