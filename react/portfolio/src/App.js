15b7f665037660d7eb69a599272ef5d82a509b52
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-09 16:50:06 +0800

    mobile layout

--- react/portfolio/src/App.js
+++ react/portfolio/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <Header>Hello</Header>
+      <Header></Header>
     </div>
   );
 }
--- react/portfolio/src/components/Header.js
+++ react/portfolio/src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Header = () => {
     return(
-        <header className='flex justify-between px-5 py-2 bg-gray-500'>
-        <a href='/' className='logo text-2xl font-bold'>Jonathan Healy</a>   
-            <nav>
-                {/* Desktop Nav */}
+        <header className='flex justify-between px-5 py-2 bg-gray-600'>
+        <a href='/' className='logo text-2xl text-white font-bold'>Jonathan Healy</a>   
+            {/* Desktop Nav */}
+            <nav className='hidden md:block'>
                 <ul className='flex'>
                     <li><a href='/#'>About</a></li>
                     <li><a href='/#projects'>Projects</a></li>
@@ -14,7 +14,20 @@ const Header = () => {
                     <li><a href='/#resume'>Resume</a></li>
                 </ul>
             </nav>
-        </header>
+
+            {/* Mobile Nav */}
+            <nav className='block md:hidden fixed left-0 right-0 top-10 py-2 h-full bg-gray-900'>
+                {/* Desktop Nav */}
+                <ul className='flex flex-col'>
+                    <li><a href='/#'>About</a></li>
+                    <li><a href='/#projects'>Projects</a></li>
+                    <li><a href='/#blog'>Blog</a></li>
+                    <li><a href='/#contact'>Contact</a></li>
+                    <li><a href='/#resume'>Resume</a></li>
+                </ul>
+            </nav>
+            {/* Toggle button */}
+        </header> 
     )
 }
 export default Header
