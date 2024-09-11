6723be96d8368e67f4027e8a6854219367bf114e
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-11 23:45:25 +0800

    navigate to default, redirect

--- react/book-library/src/App.js
+++ react/book-library/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import { BooksProvider } from "./BooksContext";
 import Books from "./components/Books";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
     <BooksProvider>
-      <Books></Books>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Books></Books>}></Route>
+          <Route path="/books" element={<Navigate to="/"></Navigate>}></Route>
+        </Routes>
+      </Router>
     </BooksProvider>
   );
 }
