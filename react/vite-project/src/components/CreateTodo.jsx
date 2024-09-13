8bcfd9bf5f31ef9e354d6ae3661ad5087f839faf
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-13 13:41:16 +0800

    ternary

--- react/vite-project/src/components/CreateTodo.jsx
+++ react/vite-project/src/components/CreateTodo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 const CreateTodo = () => {
+  const tasks = 2;
+  const countTasks = () => {
+    // if (tasks === 0) {
+    //   return "No Tasks Available";
+    // } else {
+    //   return `Tasks: ${tasks}`;
+    // }
+    return tasks === 0 ? "No Tasks Available" : `Tasks: ${tasks}`;
+  };
   return (
     <>
-      <h1>Create new todo from here</h1>
+      <h1>{countTasks()}</h1>
       <button>Add Task</button>
     </>
   );
