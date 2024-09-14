4b4745567c84007b672c0121cdc3b29c042c6eaa
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-14 19:10:08 +0800

    add docstring

---
+++ solidity/first-contract.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message; 
+    function Inbox(string initialMessage) public {
+        /**
+         * @notice Initializes the Inbox contract with an initial message.
+         * @param initialMessage The initial message to be stored.
+         */
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        /**
+         * @notice Updates the message stored in the Inbox contract.
+         * @param newMessage The new message to be stored.
+         */
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string) {
+        /**
+         * @notice Returns the current message stored in the Inbox contract.
+         */
+        return message;
+    }
+}
