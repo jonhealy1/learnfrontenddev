4202df725c0237adeac51789a53b63215174d21a
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-16 14:39:22 +0800

    add styling

--- react/task-trek/src/components/TaskForm.jsx
+++ react/task-trek/src/components/TaskForm.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./TaskForm.css";
+
 const TaskForm = () => {
   return (
     <header className="app_header">
@@ -7,22 +9,26 @@ const TaskForm = () => {
         <input
           type="text"
           className="task_input"
-          placeholder="Enter your tasks"
+          placeholder="Enter your task"
         />
         <div className="task_form_bottom_line">
-          <button className="tag">HTML</button>
-          <button className="tag">CSS</button>
-          <button className="tag">JavaScript</button>
-          <button className="tag">React</button>
+          <div>
+            <button className="tag">HTML</button>
+            <button className="tag">CSS</button>
+            <button className="tag">JavaScript</button>
+            <button className="tag">React</button>
+          </div>
+          <div>
+            <select className="task_status">
+              <option value="todo">To do</option>
+              <option value="doing">Doing</option>
+              <option value="done">Done</option>
+            </select>
 
-          <select className="task_status">
-            <option value="todo">To do</option>
-            <option value="doing">Doing</option>
-            <option value="done">Done</option>
-          </select>
-          <button type="submit" className="task_submit">
-            + Add Task
-          </button>
+            <button type="submit" className="task_submit">
+              + Add Task
+            </button>
+          </div>
         </div>
       </form>
     </header>
