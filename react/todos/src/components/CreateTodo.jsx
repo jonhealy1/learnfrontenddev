a45d05ba22cfbc7484f620f4f9a7bd4ed73549ac
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-16 11:49:54 +0800

    mapping list

--- react/todos/src/components/CreateTodo.jsx
+++ react/todos/src/components/CreateTodo.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 const CreateTodo = () => {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
+  const tasks = ["Task 1", "Task 2", "Task 3"];
 
   const handleClick = () => {
     setCount(count + 1);
@@ -20,6 +21,9 @@ const CreateTodo = () => {
         Add Task
       </button>
       <h1>Input: {input}</h1>
+      {tasks.map((task) => (
+        <li key={task}>{task}</li>
+      ))}
     </>
   );
 };
