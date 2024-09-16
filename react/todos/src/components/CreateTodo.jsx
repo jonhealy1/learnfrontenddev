784a5c451b15082b0882a39b2a530affdf3dcdc8
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-16 11:30:53 +0800

    handling user inputs

--- react/todos/src/components/CreateTodo.jsx
+++ react/todos/src/components/CreateTodo.jsx
@@ -3,16 +3,23 @@ import { useState } from "react";
 
 const CreateTodo = () => {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
 
   const handleClick = () => {
     setCount(count + 1);
   };
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
   return (
     <>
       <h1>Tasks: {count}</h1>
+      <input type="text" onChange={handleChange} />
       <button onClick={handleClick} value="Add Task Button">
         Add Task
       </button>
+      <h1>Input: {input}</h1>
     </>
   );
 };
