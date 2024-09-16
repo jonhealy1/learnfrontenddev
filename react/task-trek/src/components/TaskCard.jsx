202b8788e2e9ec5e192a0bc5ab4eed7446c1cfc7
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-17 00:29:45 +0800

    finish styling

--- react/task-trek/src/components/TaskCard.jsx
+++ react/task-trek/src/components/TaskCard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./TaskCard.css";
+import Tag from "./Tag";
+import deleteIcon from "../assets/delete.png";
 
 const TaskCard = () => {
-  return <div>TaskCard</div>;
+  return (
+    <article className="task_card">
+      <p className="task_text">This is sample text.</p>
+      <div className="task_card_bottom_line">
+        <div className="task_card_tags">
+          <Tag tagName="HTML"></Tag>
+          <Tag tagName="CSS"></Tag>
+        </div>
+        <div className="task_delete"></div>
+        <img src={deleteIcon} className="delete_icon" alt="" />
+      </div>
+    </article>
+  );
 };
 
 export default TaskCard;
--- react/task-trek/src/components/TaskColumn.jsx
+++ react/task-trek/src/components/TaskColumn.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Todo from "../assets/direct-hit.png";
 
 import "./TaskColumn.css";
+import TaskCard from "./TaskCard";
 
 const TaskColumn = ({ image, title }) => {
   return (
@@ -10,6 +11,7 @@ const TaskColumn = ({ image, title }) => {
         <img className="task_column_icon" src={image} alt="" />
         {title}
       </h2>
+      <TaskCard></TaskCard>
     </section>
   );
 };
