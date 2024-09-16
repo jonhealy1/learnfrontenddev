dbd3dcaef493acad1242dadc7430eeaa05f5acd1
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-16 23:21:39 +0800

    create task card

--- react/task-trek/src/components/TaskColumn.jsx
+++ react/task-trek/src/components/TaskColumn.jsx
@@ -3,12 +3,12 @@ import Todo from "../assets/direct-hit.png";
 
 import "./TaskColumn.css";
 
-const TaskColumn = (props) => {
+const TaskColumn = ({ image, title }) => {
   return (
     <section className="task_column">
       <h2 className="task_column_heading">
-        <img className="task_column_icon" src={props.image} alt="" />
-        {props.title}
+        <img className="task_column_icon" src={image} alt="" />
+        {title}
       </h2>
     </section>
   );
