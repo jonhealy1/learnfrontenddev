55103a439ea6c7aa644674fd434516924c4b68e3
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-17 14:21:24 +0800

    set up compile

--- solidity/inbox/contracts/Inbox.sol
+++ solidity/inbox/contracts/Inbox.sol
@@ -2,3 +2,23 @@ pragma solidity ^0.4.17;
 // linter warnings (red underline) about pragma version can igonored!
 
 // contract code will go here
+contract Inbox {
+    
+    string public message; 
+
+    function Inbox(string initialMessage) public {
+        /**
+         * @notice Initializes the Inbox contract with an initial message.
+         * @param initialMessage The initial message to be stored.
+         */
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        /**
+         * @notice Updates the message stored in the Inbox contract.
+         * @param newMessage The new message to be stored.
+         */
+        message = newMessage;
+    }
+}
