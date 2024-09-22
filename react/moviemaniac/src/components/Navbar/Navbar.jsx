fa7c20803dd6e939038e2764eac9f214c5da07ef
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-22 12:30:20 +0800

    navbar styling

--- react/moviemaniac/src/components/Navbar/Navbar.jsx
+++ react/moviemaniac/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
 import "./Navbar.css";
+import Fire from "../../assets/fire.png";
+import Star from "../../assets/glowing-star.png";
+import Party from "../../assets/partying-face.png";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>MovieManiac</h1>
-      <div className="navbar-links">
-        <a href="">Popular</a>
+      <div className="navbar_links">
+        <a href="">
+          Popular <img className="navbar_emoji" src={Fire} alt="fire emoji" />
+        </a>
+        <a href="">
+          Top Rated <img className="navbar_emoji" src={Star} alt="star emoji" />
+        </a>
+        <a href="">
+          Upcoming
+          <img className="navbar_emoji" src={Party} alt="party emoji" />
+        </a>
       </div>
     </nav>
   );
