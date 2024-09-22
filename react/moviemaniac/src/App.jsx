23e61c0a1a410ab1b31db2ff2158a2a4935e479f
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-22 11:59:36 +0800

    add assets basic navbar

--- react/moviemaniac/src/App.jsx
+++ react/moviemaniac/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div className="app">
-      <nav className="navbar">Navbar content</nav>
+      <Navbar />
 
       <main>Main Content</main>
     </div>
---
+++ react/moviemaniac/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>MovieManiac</h1>
+      <div className="navbar-links">
+        <a href="">Popular</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
