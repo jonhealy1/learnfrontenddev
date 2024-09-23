d624273905ef0d0078338d042b907b1d30740a77
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-23 19:16:18 +0800

    movie details skeleton

--- react/moviemaniac/src/components/MovieList/MovieCard.jsx
+++ react/moviemaniac/src/components/MovieList/MovieCard.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import "./MovieCard.css";
+import Star from "../../assets/star.png";
 
 const MovieCard = () => {
   return (
     <a href="" className="movie_card">
       <img
-        src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/&psig=AOvVaw23n0tqg7D3FYZzPAH_oBZW&ust=1727159452553000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPCv3M642IgDFQAAAAAdAAAAABAp"
+        src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg"
         alt="movie_poster"
         className="movie_poster"
       />
+      <div className="movie_details">
+        <h3 className="movie_details_heading">Movie Name</h3>
+        <div className="movie_date_rate">
+          <p>10-20-2020</p>
+          <p>
+            8.1 <img src={Star} alt="rating icon" className="card_emoji" />
+          </p>
+        </div>
+        <p className="movie_description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia at,
+          nulla aliquid facilis cupiditate pariatur.
+        </p>
+      </div>
     </a>
   );
 };
