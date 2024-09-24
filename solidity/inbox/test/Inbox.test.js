738467de59fe7c0f13eabc7322c1f224e4a82320
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-24 14:00:28 +0800

    mock mocha tests

--- solidity/inbox/test/Inbox.test.js
+++ solidity/inbox/test/Inbox.test.js
@@ -1,6 +1,23 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const ganache = require("ganache");
+const { Web3 } = require("web3");
 
-// updated ganache and web3 imports added for convenience
+const assert = require("assert");
 
 // contract test code will go here
+const web3 = new Web3(ganache.provider());
+
+class Car {
+  park() {
+    return "stopped";
+  }
+  drive() {
+    return "vroom";
+  }
+}
+
+describe("Car", () => {
+  it("can park", () => {
+    const car = new Car();
+    assert.equal(car.park(), "stopped");
+  });
+});
