4b8344e657055358cc68faa7d093cc47d5792cf1
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-25 00:18:27 +0800

    use web3 promise

--- solidity/inbox/test/Inbox.test.js
+++ solidity/inbox/test/Inbox.test.js
@@ -2,27 +2,20 @@ const ganache = require("ganache");
 const { Web3 } = require("web3");
 
 const assert = require("assert");
+const { isTypedArray } = require("util/types");
 
 // contract test code will go here
 const web3 = new Web3(ganache.provider());
 
-class Car {
-  park() {
-    return "stopped";
-  }
-  drive() {
-    return "vroom";
-  }
-}
-
-let car;
-
 beforeEach(() => {
-  car = new Car();
+  // Get a list of all accounts
+  web3.eth.getAccounts().then((fetchedAccounts) => {
+    console.log(fetchedAccounts);
+  });
+
+  // Use one of those accounts to deploy the contract
 });
 
-describe("Car", () => {
-  it("can park", () => {
-    assert.equal(car.park(), "stopped");
-  });
+describe("Inbox", () => {
+  it("deploys a contract", () => {});
 });
