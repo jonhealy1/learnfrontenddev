5651561ba3b777f73ef6426c291424a8b4c9bc4a
jonhealy1 <jonathan.d.healy@gmail.com>
2024-09-25 00:39:44 +0800

    re jig test

--- solidity/inbox/test/Inbox.test.js
+++ solidity/inbox/test/Inbox.test.js
@@ -2,22 +2,30 @@ const ganache = require("ganache");
 const { Web3 } = require("web3");
 
 const assert = require("assert");
-const { isTypedArray } = require("util/types");
+const { interface, bytecode } = require("../compile");
 
 // contract test code will go here
 const web3 = new Web3(ganache.provider());
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy the contract
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(accounts);
+    console.log(inbox);
   });
 });
