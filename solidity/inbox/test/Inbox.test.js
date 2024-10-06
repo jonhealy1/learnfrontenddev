e49cb749a36e87824082f9bf5733c9359f3df8b1
jonhealy1 <jonathan.d.healy@gmail.com>
2024-10-06 18:38:36 +0800

    test default message

--- solidity/inbox/test/Inbox.test.js
+++ solidity/inbox/test/Inbox.test.js
@@ -7,6 +7,8 @@ const { interface, bytecode } = require("../compile");
 // contract test code will go here
 const web3 = new Web3(ganache.provider());
 
+const INITIAL_MESSAGE = "Hi There!";
+
 let accounts;
 let inbox;
 
@@ -19,7 +21,7 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ["Hi there!"],
+      arguments: [INITIAL_MESSAGE],
     })
     .send({ from: accounts[0], gas: "1000000" });
 });
@@ -28,5 +30,10 @@ describe("Inbox", () => {
   it("deploys a contract", () => {
     assert.ok(inbox.options.address);
     console.log(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
   });
 });
