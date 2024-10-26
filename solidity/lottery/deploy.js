9d21ba1c03ebab80147edcc4ff5df398fd3374ef
jonhealy1 <jonathan.d.healy@gmail.com>
2024-10-26 20:25:59 +0800

    update gas price

--- solidity/lottery/deploy.js
+++ solidity/lottery/deploy.js
@@ -1,26 +1,29 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
-  // remember to change this to your own phrase!
-  'REPLACE WITH YOUR INFURA URL'
-  // remember to change this to your own endpoint!
+  "property bacon season focus acquire sugar bind trick empty behave plastic siren",
+  "https://sepolia.infura.io/v3/ad9567965c6246dd83b7a41a3edb2257"
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+    .send({ gas: "500000", from: accounts[0] });
 
   console.log(interface);
   console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
 };
 deploy();
+
+Attempting to deploy from account 0xF2e325a9E34E41c4b139C09aC46D32175c0e0088
+[{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+Contract deployed to 0xEf47591eEE8A3Db3F56ff0376EB8c743Ac0De6A6
