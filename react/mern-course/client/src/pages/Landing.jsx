10e44f6967783f11d46552fb06e18df8579b89fd
jonhealy1 <jonathan.d.healy@gmail.com>
2024-10-29 15:14:18 +0800

    add error page

--- react/mern-course/client/src/pages/Landing.jsx
+++ react/mern-course/client/src/pages/Landing.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import styled from "styled-components";
+
+const StyledBtn = styled.button`
+  font-size: 1.5rem;
+  background: red;
+  color: white;
+`;
 
 const Landing = () => {
   return <h1>Landing</h1>;
