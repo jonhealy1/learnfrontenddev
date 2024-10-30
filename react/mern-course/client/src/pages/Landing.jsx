bad8078826098f8266834b8de8034bc57935ba5b
jonhealy1 <jonathan.d.healy@gmail.com>
2024-10-30 11:24:03 +0800

    style landing page

--- react/mern-course/client/src/pages/Landing.jsx
+++ react/mern-course/client/src/pages/Landing.jsx
@@ -1,17 +1,28 @@
 import styled from "styled-components";
-
-const StyledBtn = styled.button`
-  font-size: 1.5rem;
-  background: red;
-  color: white;
-`;
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <StyledBtn>styled btn</StyledBtn>
-    </div>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
+            vero ea dolores excepturi minima, optio error quod autem commodi ex
+            suscipit dolorem, earum soluta nihil, impedit fugiat eos accusantium
+            voluptatem.
+          </p>
+        </div>
+      </div>
+    </Wrapper>
   );
 };
 
