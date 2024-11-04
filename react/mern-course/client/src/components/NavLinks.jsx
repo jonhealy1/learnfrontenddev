70e893fb84f3ca81cb7871f39642d0eede324aae
jonhealy1 <jonathan.d.healy@gmail.com>
2024-11-04 15:20:42 +0800

    create nav links component

---
+++ react/mern-course/client/src/components/NavLinks.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useDashboardContext } from "../pages/DashboardLayout";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+
+export const NavLinks = () => {
+  const { toggleSidebar, user } = useDashboardContext();
+  return (
+    <div className="nav-links">
+      {links.map((link) => {
+        const { text, path, icon } = link;
+        return (
+          <NavLink
+            to={path}
+            key={text}
+            className="nav-link"
+            onClick={toggleSidebar}
+          >
+            <span className="icon">{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavLinks;
