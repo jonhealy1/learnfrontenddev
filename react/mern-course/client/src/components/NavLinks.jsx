86bd8529eae3f99dd26a20193d2cd157b2d66f23
jonhealy1 <jonathan.d.healy@gmail.com>
2024-11-04 15:37:47 +0800

    big sidebar

--- react/mern-course/client/src/components/NavLinks.jsx
+++ react/mern-course/client/src/components/NavLinks.jsx
@@ -3,7 +3,7 @@ import { useDashboardContext } from "../pages/DashboardLayout";
 import links from "../utils/links";
 import { NavLink } from "react-router-dom";
 
-export const NavLinks = () => {
+export const NavLinks = ({ isBigSidebar }) => {
   const { toggleSidebar, user } = useDashboardContext();
   return (
     <div className="nav-links">
@@ -14,7 +14,7 @@ export const NavLinks = () => {
             to={path}
             key={text}
             className="nav-link"
-            onClick={toggleSidebar}
+            onClick={isBigSidebar ? null : toggleSidebar}
           >
             <span className="icon">{icon}</span>
             {text}
