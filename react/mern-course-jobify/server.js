ad647b60b766428d153ad43856b4e7bfc98df21f
jonhealy1 <jonathan.d.healy@gmail.com>
2024-11-07 13:31:33 +0800

    add basic job crud

--- react/mern-course-jobify/server.js
+++ react/mern-course-jobify/server.js
@@ -10,6 +10,17 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
+import { nanoid } from "nanoid";
+
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
+});
+
 app.post("/", (req, res) => {
   console.log(req);
 
@@ -24,4 +35,47 @@ const port = process.env.PORT || 5100;
 
 app.listen(port, () => {
   console.log(`server running on PORT ${port}...`);
+});
+
+// CREATE JOB
+
+app.post("/api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  }
+  const id = nanoid(10);
+  // console.log(id);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(200).json({ job });
+});
+
+// GET SINGLE JOB
+
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ job });
+});
+
+// EDIT JOB
+
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: "job modified", job });
 });
