bde6865585eac7dfb068db201bb3695883562340
jonhealy1 <jonathan.d.healy@gmail.com>
2024-11-07 11:52:24 +0800

    add simple post request

--- react/mern-course-jobify/server.js
+++ react/mern-course-jobify/server.js
@@ -1,6 +1,14 @@
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  console.log(req);
+
+  res.json({ message: "Data received", data: req.body });
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
