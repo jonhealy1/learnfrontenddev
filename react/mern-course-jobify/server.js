0cce0d241ef88fbbc950fd856c8151c3dc05eb62
jonhealy1 <jonathan.d.healy@gmail.com>
2024-11-07 14:15:29 +0800

    add delete, global 404

--- react/mern-course-jobify/server.js
+++ react/mern-course-jobify/server.js
@@ -79,3 +79,26 @@ app.patch("/api/v1/jobs/:id", (req, res) => {
   job.position = position;
   res.status(200).json({ msg: "job modified", job });
 });
+
+// DELETE JOB
+
+app.delete("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+
+  res.status(200).json({ msg: "job deleted" });
+});
+
+app.use("*", (req, res) => {
+  res.status(404), json({ msg: "not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500), json({ msg: "something went wrong" });
+});
